6ba7e1d1016808688c7f7127cbd24d968531f80b
sonyakun <owner@sonyakun.com>
2024-03-19 01:52:55 +0900

    __init__.pyが相対インポートになるように

--- mitypes/__init__.py
+++ mitypes/__init__.py
@@ -1,5 +1,5 @@
-from channel import channel
-from drive import DriveFile, DriveFolder, df_property
-from note import Note
-from poll import Poll
-from user import User, UserLite
+from .channel import channel
+from .drive import DriveFile, DriveFolder, df_property
+from .note import Note
+from .poll import Poll
+from .user import User, UserLite
